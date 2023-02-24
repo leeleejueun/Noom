10b50660b84e17049ecd60c5fafb9c6e65876615
leeleejueun <leejueun413@gmail.com>
2023-02-24 18:23:28 +0900

    Noomtest

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,13 @@
 //frontend
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type,payload){
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
 
 function handleOpen() {
   console.log("Connected to Server ✅");
@@ -7,14 +15,31 @@ function handleOpen() {
 
 socket.addEventListener("open", handleOpen);
 
-socket.addEventListener("message", message => {
-  console.log("New message: ", message.data);
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected to Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 1000);
+function handleSubmit(event){
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message",input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event){
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send({ 
+    type: "nickname",
+    payload: input.value,
+  });
+} 
+
+messageForm.addEventListener("submit",handleSubmit);
+nickForm.addEventListener("submit",handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -21,13 +21,15 @@ function onSocketClose() {
   console.log("Disconnected from the Browser ❌");
 }
 
-wss.on("connection", socket => {
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
   socket.on("close", onSocketClose);
-  socket.on("message", message => {
-    console.log(message.toString("utf8"));
+  socket.on("message", (message)=>{
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
